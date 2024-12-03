f8c0b5dde9dcfef60ce0fe8b15211f5722ec9154
oscarkilhed <oscar.kilhed@grafana.com>
2024-12-03 15:10:27 +0100

    first

---
+++ src/module.tsx
@@ -0,0 +1,39 @@
+import React, { Suspense, lazy } from 'react';
+import { AppPlugin, type AppRootProps } from '@grafana/data';
+import { LoadingPlaceholder } from '@grafana/ui';
+import type { AppConfigProps } from './components/AppConfig/AppConfig';
+import { fileHandler } from 'handlers/fileHandler';
+
+const LazyApp = lazy(() => import('./components/App/App'));
+const LazyAppConfig = lazy(() => import('./components/AppConfig/AppConfig'));
+
+const App = (props: AppRootProps) => (
+  <Suspense fallback={<LoadingPlaceholder text="" />}>
+    <LazyApp {...props} />
+  </Suspense>
+);
+
+const AppConfig = (props: AppConfigProps) => (
+  <Suspense fallback={<LoadingPlaceholder text="" />}>
+    <LazyAppConfig {...props} />
+  </Suspense>
+);
+
+export const plugin = new AppPlugin<{}>()
+  .setRootPage(App)
+  .addConfigPage({
+    title: 'Configuration',
+    icon: 'cog',
+    body: AppConfig,
+    id: 'configuration',
+  })
+  .addHook({
+    title: 'foo',
+    targets: ['dashboard/grid'],
+    //@ts-ignore
+    hook: (data) => {
+      if (data instanceof File) {
+        return fileHandler(data);
+      }
+    },
+  });
